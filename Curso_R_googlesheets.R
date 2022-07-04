3b2643677018c330f8b30b173dc2238887651041
Leonardo MECON <lcalcagno@mecon.gob.ar>
2022-07-04 17:25:38 -0300

    Creación fichero R con paquetes

---
+++ Curso_R_googlesheets.R
@@ -0,0 +1,21 @@
+
+# Packages -----------------
+install.packages("tidyverse")
+install.packages("readxl")
+install.packages("openxlsx")
+install.packages('questionr')
+install.packages('readr')
+install.packages('googlesheets4')
+install.packages('googledrive')
+
+rm(list=ls())
+gc()
+
+# Open packages
+library(tidyverse)
+library(readxl)
+library(openxlsx)
+library(questionr)
+library(readr)
+library(googlesheets4)
+library(googledrive)
